b2dc475671fc0a6a990a6adc64f21937a3891a3b
Svitlana Ruts <160260280+svitlana-ruts@users.noreply.github.com>
2024-05-23 10:11:12 +0200

    adds

--- js/task-1.js
+++ js/task-1.js
@@ -16,7 +16,6 @@ function slugify(title) {
   return title.toLowerCase().split(' ').join('-');
 }
     
-
 console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
 console.log(slugify("English for developer")); // "english-for-developer"
 console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
--- js/task-3.js
+++ js/task-3.js
@@ -10,13 +10,11 @@
 
 function filterArray(numbers, value) {
     let filteredArray = [];
-    
     for (let i = 0; i < numbers.length; i++) {
         if (numbers[i] > value) {
             filteredArray.push(numbers[i]);
         }
     }
-    
     return filteredArray;
 }
 
